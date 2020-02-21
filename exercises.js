6ce12d4eb0f00d62230b7c7128471f33d77043b9
Hon <dennis.mejen@gmail.com>
2020-02-21 16:55:30 +0700

    Add answers

--- exercises.js
+++ exercises.js
@@ -17,3 +17,39 @@ while (key !== "stop" || value !== "stop") {
 }
 
 console.log(obj);
+
+// Ex2
+let student = {};
+let firstName = "first name";
+let lastName = "last name";
+let age = "age";
+let height = "height";
+
+student[firstName] = "Metha";
+student[lastName] = "Jenvitayaroj";
+student[age] = 29;
+student[height] = 175;
+
+console.log(student);
+
+// Ex3
+let basket = {};
+
+while (true) {
+  let fruit = prompt("Enter fruit:");
+  if (fruit === "stop") {
+    break;
+  }
+  let amount = prompt("Enter amount:");
+  if (amount === "stop") {
+    break;
+  }
+  console.log(`${fruit} + ${amount}`);
+  if (amount > 1) {
+    basket[fruit + "s"] = parseInt(amount);
+  } else {
+    basket[fruit] = parseInt(amount);
+  }
+}
+
+console.log(basket);

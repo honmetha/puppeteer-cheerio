9cd23aef8e334f324b26a642b0fe7a5105ee9e9a
Hon <dennis.mejen@gmail.com>
2020-11-21 22:21:45 +0700

    [Added] Add click and type

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.goto('https://www.google.com');
+  await page.waitForSelector('div form div:nth-child(2) input');
+  await page.click('div form div:nth-child(2) input');
+  await page.keyboard.type('puppeteer docs');
+  await page.keyboard.press('Enter');
   // await page.screenshot({path: 'google.png'});
 
-  // await browser.close();
+  await setTimeout(() => { browser.close(); }, 4000);
 })();
